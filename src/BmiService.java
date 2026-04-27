4f393e23379ea0d8e2482fe2c4c94e078be3af2d
Ksenya Kos <kskosyrevavv@gmail.com>
2026-04-27 13:14:30 +0300

    Changed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate (double bodyMass, double height) {
-        double index = bodyMass / (height * height);
+    public double calculate (double bodyMassKg, double heightMetre) {
+        double index = bodyMassKg / (heightMetre * heightMetre);
         return index;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bodyMass = 98.00;
-        double height = 1.85;
+        double bodyMassKg = 98.00;
+        double heightMetre = 1.85;
         double index = service.calculate(98.00, 1.85);
         System.out.println((int) index);
     }
