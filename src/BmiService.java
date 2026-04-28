ca24b539e9f657d41a6748a5fe489ac06455b889
Ksenya Kos <kskosyrevavv@gmail.com>
2026-04-28 16:05:24 +0300

    changed return

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate (double bodyMassKg, double heightMetre) {
         double index = bodyMassKg / (heightMetre * heightMetre);
-        return index;
+        return (int)index;
     }
 }
